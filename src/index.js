a836c531e8990c2a8d74ea27cfa93db4b6f6acd1
CarlosPerez <carlosmperez2901@gmail.com>
2025-03-28 17:08:35 -0700

    Add the about page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import "./styles.css";
-import { createHomePage } from "./homePage";
 
+import { createHomePage } from "./homePage";
+import { createAboutPage } from "./aboutMePage";
+
+
+const contentDiv = document.querySelector("div#content");
 
 const homeButton = document.querySelector("button#home");
 const menuButton = document.querySelector("button#menu");
 const aboutButton = document.querySelector("button#about");
 
-homeButton.addEventListener("click", createHomePage);
 
+homeButton.addEventListener("click", () => {
+  deleteContent(contentDiv);
+  createHomePage();
+});
+
+aboutButton.addEventListener("click", () => {
+  deleteContent(contentDiv);
+  createAboutPage();
+});
+
+function deleteContent(element) {
+  element.innerHTML = "";
+}
 
